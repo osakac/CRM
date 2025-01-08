557063eaae4251af2f36f984d800db95b0215a24
osakac <shinmoriof@gmail.com>
2025-01-08 12:56:47 +0400

    init: wrapper component for modals in the app

---
+++ src/components/modal/Modals/Modals.vue
@@ -0,0 +1,17 @@
+<template>
+  <Teleport to="body">
+    <component
+      v-for="modal in appStore.modals"
+      :key="modal.id"
+      :is="modal.component"
+      :is-visible="modal.isVisible"
+      @close="appStore.closeModal(modal.id)"
+    ></component>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+import { useAppStore } from "@/store/appStore";
+
+const appStore = useAppStore();
+</script>
