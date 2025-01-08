ce628a833f0a5b66507269643e5fcdb7ac960cde
osakac <shinmoriof@gmail.com>
2025-01-08 12:57:52 +0400

    init: table wrapper

---
+++ src/components/table/TheTable.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="mb-10">
+    <TableHeader />
+    <TableContent />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TableContent from "@/components/table/TableContent.vue";
+import TableHeader from "@/components/table/TableHeader.vue";
+</script>
