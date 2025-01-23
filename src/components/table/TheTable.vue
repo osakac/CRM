b28a3bb781f905ce24508cd50b203faa66721f35
osakac <shinmoriof@gmail.com>
2025-01-23 19:55:39 +0400

    add: connect with client store

--- src/components/table/TheTable.vue
+++ src/components/table/TheTable.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="mb-10">
     <TableHeader />
-    <TableContent />
+    <TableContent :clients="clientStore.clients" />
   </div>
 </template>
 
 <script setup lang="ts">
-import TableContent from "@/components/table/TableContent.vue";
+import TableContent from "@/components/table/TableContent/TableContent.vue";
 import TableHeader from "@/components/table/TableHeader.vue";
+import { useClientStore } from "@/store/client/clientStore";
+
+const clientStore = useClientStore();
 </script>
