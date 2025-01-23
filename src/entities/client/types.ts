264f89e3ee8822eb15fc9a4eefd9f3ae154c99b7
osakac <shinmoriof@gmail.com>
2025-01-23 19:48:23 +0400

    add: updateClientData function type and optional secondName

--- src/entities/client/types.ts
+++ src/entities/client/types.ts
@@ -1,11 +1,14 @@
+import type { TClientSchema } from "@/helpers/Client/validateConfig";
+
 export interface IClient {
   id: number;
   name: string;
   surname: string;
-  secondName: string;
+  secondName: string | undefined;
   dateCreated: Date;
   dateUpdated: Date;
   contacts: IClientContact[];
+  updateClientData: (clientData: TClientSchema) => void;
 }
 
 export interface IClientContact {
