f1e259a94b84586ebf323b1eaeb4179aed81f900
osakac <shinmoriof@gmail.com>
2025-01-23 19:43:00 +0400

    add: update client info function

--- src/store/client/clientStore.ts
+++ src/store/client/clientStore.ts
@@ -13,12 +13,27 @@ export const useClientStore = defineStore("clientStore", () => {
       secondName: "Юрьевич",
       dateCreated: new Date(),
       dateUpdated: new Date(),
-      contacts: [],
+      contacts: [
+        {
+          id: 1,
+          contactType: "phone",
+          contactValue: "+7 (999) 999-99-99",
+        },
+      ],
+      updateClientData(clientData: TClientSchema) {
+        this.name = clientData.name;
+        this.surname = clientData.surname;
+        this.secondName = clientData.secondName;
+        this.contacts = clientData.contacts;
+        this.dateUpdated = new Date();
+      },
     },
   ]);
 
-  const addClient = (client: TClientSchema) => clients.value.push(new Client(client));
+  const addClient = (clientData: TClientSchema) => clients.value.push(new Client(clientData));
   const deleteClient = (clientId: number) => (clients.value = clients.value.filter((c) => c.id !== clientId));
+  const updateClient = (clientId: number, clientData: TClientSchema) =>
+    clients.value.find((c) => c.id === clientId)?.updateClientData(clientData);
 
-  return { clients, addClient, deleteClient };
+  return { clients, addClient, deleteClient, updateClient };
 });
