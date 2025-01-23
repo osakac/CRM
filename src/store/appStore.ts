3ba2f1a633b814ffe75fa2edc88c0e719ca9bbdb
osakac <shinmoriof@gmail.com>
2025-01-23 19:45:49 +0400

    change: open modal logic

--- src/store/appStore.ts
+++ src/store/appStore.ts
@@ -1,13 +1,24 @@
 import ClientModal from "@/components/modal/ClientModal/ClientModal.vue";
-import { Modal } from "@/entities/modal";
+import { Modal } from "@/entities/modal/modal";
 import { defineStore } from "pinia";
 import { ref, shallowRef } from "vue";
+import { useClientStore } from "./client/clientStore";
 
 export const useAppStore = defineStore("appStore", () => {
-  const modals = ref([new Modal("clientModal", shallowRef(ClientModal))]);
-  const getModals = () => modals;
-  const openModal = (id: string) => modals.value.find((m) => m.id === id)?.openModal();
+  const modals = ref<Modal[]>([new Modal("clientModal", shallowRef(ClientModal))]);
+  const openModal = (modalId: string, clientId?: number) => {
+    const modal = modals.value.find((m) => m.id === modalId);
+
+    if (clientId && modal) {
+      const clientStore = useClientStore();
+      const clientProps = clientStore.clients.find((c) => c.id === clientId);
+      if (clientProps) modal.clientData = clientProps;
+    }
+
+    modal?.openModal();
+  };
+
   const closeModal = (id: string) => modals.value.find((m) => m.id === id)?.closeModal();
 
-  return { modals, getModals, openModal, closeModal };
+  return { modals, openModal, closeModal };
 });
