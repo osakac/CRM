bee9d81c8ff75bfa6a13c7dc349b70c51797459f
osakac <shinmoriof@gmail.com>
2025-01-23 13:40:42 +0400

    add: configure floating vue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
+import FloatingVue from "floating-vue";
+import "floating-vue/dist/style.css";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
 
-createApp(App).use(createPinia()).mount("#app");
+createApp(App)
+  .use(createPinia())
+  .use(FloatingVue, {
+    themes: {
+      "custom-tooltip": {
+        $extend: "tooltip",
+        $resetCss: true,
+        distance: 10,
+      },
+    },
+  })
+  .mount("#app");
