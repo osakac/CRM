ac31e9026117cc5bbdfbe2e805f7a7bca614aab9
osakac <shinmoriof@gmail.com>
2025-01-24 13:59:36 +0400

    add: clients sorting

--- src/components/table/TheTable.vue
+++ src/components/table/TheTable.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="mb-10">
-    <TableHeader />
-    <TableContent :clients="clientStore.clients" />
+    <TableHeader
+      @sort-by-name="sortBy = 'fullName'"
+      @sort-by-date-created="sortBy = 'dateCreated'"
+      @sort-by-date-updated="sortBy = 'dateUpdated'"
+    />
+    <TableContent :clients="clientsComputed" />
   </div>
 </template>
 
 <script setup lang="ts">
 import TableContent from "@/components/table/TableContent/TableContent.vue";
-import TableHeader from "@/components/table/TableHeader.vue";
+import TableHeader from "@/components/table/TableHeader/TableHeader.vue";
+import { sortFunction, type TSortBy } from "@/helpers/Table/sortFunction";
 import { useClientStore } from "@/store/client/clientStore";
+import { computed, ref } from "vue";
 
 const clientStore = useClientStore();
+
+const sortBy = ref<TSortBy>("fullName");
+const clientsComputed = computed(() => {
+  const clients = clientStore.clients.value.toSorted(sortFunction(sortBy.value));
+  return clients;
+});
 </script>
