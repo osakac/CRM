eb0b520c804bc7b561071aa12e11960453b483c6
osakac <shinmoriof@gmail.com>
2025-01-26 22:33:11 +0400

    add: format name

--- src/helpers/Client/validateConfig.ts
+++ src/helpers/Client/validateConfig.ts
@@ -20,10 +20,10 @@ export type TClientSchema = InferType<typeof clientSchema>;
 
 export const formatClientData = (values: TClientSchema) => {
   values = clientSchema.cast(values);
-  values.name = values.name.charAt(0).toUpperCase() + values.name.slice(1);
-  values.surname = values.surname.charAt(0).toUpperCase() + values.surname.slice(1);
+  values.name = values.name.charAt(0).toUpperCase() + values.name.slice(1).toLowerCase();
+  values.surname = values.surname.charAt(0).toUpperCase() + values.surname.slice(1).toLowerCase();
   values.secondName = values.secondName
-    ? values.secondName.charAt(0).toUpperCase() + values.secondName.slice(1)
+    ? values.secondName.charAt(0).toUpperCase() + values.secondName.slice(1).toLowerCase()
     : undefined;
   return values;
 };
